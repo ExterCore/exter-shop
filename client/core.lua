4fec8de54666e7e26a6a6f2d50c76ef8407049ca
CtrlAltDelete4413 <95325881+CtrlAltDelete4413@users.noreply.github.com>
2025-05-09 14:05:55 +0700

    code cleanup

--- client/core.lua
+++ client/core.lua
@@ -12,7 +12,6 @@ Citizen.CreateThread(function()
     end
 end)
 
-
 function TriggerCallback(name, cb, ...)
     EXTER.ServerCallbacks[name] = cb
     TriggerServerEvent('exter-rentacarv2:server:triggerCallback', name, ...)
@@ -24,8 +23,6 @@ RegisterNetEvent('exter-rentacarv2:client:triggerCallback', function(name, ...)
         EXTER.ServerCallbacks[name] = nil
     end
 end)
-
-
 
 function GetPlayer(source)
     if CoreName == "qb-core" or CoreName == "qbx_core" then
@@ -46,7 +43,6 @@ function GetPlayerCid(source)
         return player.getIdentifier()
     end
 end
-
 
 function Notify(source, text, length, type)
     if CoreName == "qb-core" or CoreName == "qbx_core" then
@@ -103,7 +99,7 @@ function AddItem(source, name, amount, metadata)
         elseif hasOx then
             return exports["ox_inventory"]:AddItem(source, name, amount, metadata)
         else
-            --CUSTOM INVENTORY ADD ITEM FUNCTION HERE
+            -- CUSTOM INVENTORY ADD ITEM FUNCTION HERE
         end
 
     end
@@ -126,7 +122,7 @@ function RemoveItem(source, name, amount, metadata)
         elseif hasOx then
             return exports["ox_inventory"]:RemoveItem(source, name, amount, metadata)
         else
-            --CUSTOM INVENTORY REMOVE ITEM FUNCTION HERE
+            -- CUSTOM INVENTORY REMOVE ITEM FUNCTION HERE
         end
     end
 end
@@ -146,14 +142,14 @@ function GetItem(source, name)
         elseif hasEsx then
             return player.getInventoryItem(name)
         elseif hasOx then
-            return exports["ox_inventory"]:GetItem(source, name,nil, false)
+            return exports["ox_inventory"]:GetItem(source, name, nil, false)
         else
-            --CUSTOM INVENTORY GET ITEM FUNCTION HERE
+            -- CUSTOM INVENTORY GET ITEM FUNCTION HERE
         end
     end
 end
 
-function GetItemCount (source,item)
+function GetItemCount(source, item)
     if CoreName == "qb-core" or CoreName == "qbx_core" then
         local player = Core.Functions.GetPlayer(source)
         return item.amount
@@ -167,19 +163,18 @@ function GetItemCount (source,item)
             return exports['qs-inventory']:GetItem(source, item).amount
         elseif hasOx or hasEsx then
             return player.getInventoryItem(item).count
-        -- elseif hasOx then
-        --     return exports["ox_inventory"]:GetItem(source, item).amount
+            -- elseif hasOx then
+            --     return exports["ox_inventory"]:GetItem(source, item).amount
         else
-            --CUSTOM INVENTORY GET ITEM COUNT FUNCTION HERE
+            -- CUSTOM INVENTORY GET ITEM COUNT FUNCTION HERE
         end
     end
 end
 
-
 function GetItems(source, name)
     if CoreName == "qb-core" or CoreName == "qbx_core" then
         local player = Core.Functions.GetPlayer(source)
-        
+
         return player.Functions.GetItemsByName(name)
     elseif CoreName == "es_extended" then
         local player = Core.GetPlayerFromId(source)
@@ -191,10 +186,10 @@ function GetItems(source, name)
             return exports['qs-inventory']:GetItemsByName(source, name)
         elseif hasOx or hasEsx then
             return player.getInventoryItem(name)
-        -- elseif hasOx then
-        --     return exports["ox_inventory"]:GetItem(source,name, nil, false)
+            -- elseif hasOx then
+            --     return exports["ox_inventory"]:GetItem(source,name, nil, false)
         else
-            --CUSTOM INVENTORY GET ITEMS BY NAME FUNCTION HERE
+            -- CUSTOM INVENTORY GET ITEMS BY NAME FUNCTION HERE
         end
     end
 end
@@ -216,12 +211,12 @@ function GetInventory(source)
         elseif hasOx then
             return exports["ox_inventory"]:GetPlayerInventory(source)
         else
-            --CUSTOM INVENTORY GET INVENTORY FUNCTION HERE
+            -- CUSTOM INVENTORY GET INVENTORY FUNCTION HERE
         end
     end
 end
 
-function ItemCheckForCraft(player,reqItem)
+function ItemCheckForCraft(player, reqItem)
     if CoreName == "qb-core" or CoreName == "qbx_core" then
         local items = player.PlayerData.items
         local tbl = {}
@@ -229,7 +224,10 @@ function ItemCheckForCraft(player,reqItem)
         for x, y in pairs(items) do
             for k, v in pairs(reqItem) do
                 if y.name == v.requredItemName then
-                    table.insert(tbl, {name = v.requredItemName, count = y.amount})
+                    table.insert(tbl, {
+                        name = v.requredItemName,
+                        count = y.amount
+                    })
                 end
             end
         end
@@ -242,7 +240,10 @@ function ItemCheckForCraft(player,reqItem)
         for x, y in pairs(items) do
             for k, v in pairs(reqItem) do
                 if y.name == v.requredItemName then
-                    table.insert(tbl, {name = v.requredItemName, count = y.count})
+                    table.insert(tbl, {
+                        name = v.requredItemName,
+                        count = y.count
+                    })
                 end
             end
         end
@@ -250,7 +251,6 @@ function ItemCheckForCraft(player,reqItem)
 
     end
 end
-
 
 function ItemCountControl(source, name, amount)
     if CoreName == "qb-core" or CoreName == "qbx_core" then
@@ -269,14 +269,15 @@ function ItemCountControl(source, name, amount)
         elseif hasOx then
             return exports["ox_inventory"]:GetItem(source, name).amount >= amount
         else
-            --CUSTOM INVENTORY ITEM COUNT CONTROL FUNCTION HERE
+            -- CUSTOM INVENTORY ITEM COUNT CONTROL FUNCTION HERE
         end
     end
 end
 
-
 function RegisterUseableItem(name)
-    while CoreReady == false do Citizen.Wait(0) end
+    while CoreReady == false do
+        Citizen.Wait(0)
+    end
     if CoreName == "qb-core" or CoreName == "qbx_core" then
         Core.Functions.CreateUseableItem(name, function(source, item)
         end)
@@ -292,8 +293,6 @@ function RegisterUseableItem(name)
     end
 end
 
-
-
 function GetVehicleProperties(vehicle)
     if CoreName == "qb-core" then
         if DoesEntityExist(vehicle) then
@@ -302,12 +301,12 @@ function GetVehicleProperties(vehicle)
             local colorPrimary, colorSecondary = GetVehicleColours(vehicle)
             if GetIsVehiclePrimaryColourCustom(vehicle) then
                 local r, g, b = GetVehicleCustomPrimaryColour(vehicle)
-                colorPrimary = { r, g, b }
+                colorPrimary = {r, g, b}
             end
 
             if GetIsVehicleSecondaryColourCustom(vehicle) then
                 local r, g, b = GetVehicleCustomSecondaryColour(vehicle)
-                colorSecondary = { r, g, b }
+                colorSecondary = {r, g, b}
             end
 
             local extras = {}
@@ -380,12 +379,8 @@ function GetVehicleProperties(vehicle)
                 windowTint = GetVehicleWindowTint(vehicle),
                 windowStatus = windowStatus,
                 doorStatus = doorStatus,
-                neonEnabled = {
-                    IsVehicleNeonLightEnabled(vehicle, 0),
-                    IsVehicleNeonLightEnabled(vehicle, 1),
-                    IsVehicleNeonLightEnabled(vehicle, 2),
-                    IsVehicleNeonLightEnabled(vehicle, 3)
-                },
+                neonEnabled = {IsVehicleNeonLightEnabled(vehicle, 0), IsVehicleNeonLightEnabled(vehicle, 1),
+                               IsVehicleNeonLightEnabled(vehicle, 2), IsVehicleNeonLightEnabled(vehicle, 3)},
                 neonColor = table.pack(GetVehicleNeonLightsColour(vehicle)),
                 interiorColor = GetVehicleInteriorColour(vehicle),
                 extras = extras,
@@ -443,7 +438,7 @@ function GetVehicleProperties(vehicle)
                 modKit47 = GetVehicleMod(vehicle, 47),
                 modLivery = modLivery,
                 modKit49 = GetVehicleMod(vehicle, 49),
-                liveryRoof = GetVehicleRoofLivery(vehicle),
+                liveryRoof = GetVehicleRoofLivery(vehicle)
             }
         else
             return
@@ -452,7 +447,7 @@ function GetVehicleProperties(vehicle)
         if not DoesEntityExist(vehicle) then
             return
         end
-    
+
         local colorPrimary, colorSecondary = GetVehicleColours(vehicle)
         local pearlescentColor, wheelColor = GetVehicleExtraColours(vehicle)
         local hasCustomPrimaryColor = GetIsVehiclePrimaryColourCustom(vehicle)
@@ -460,56 +455,57 @@ function GetVehicleProperties(vehicle)
         local interiorColor = GetVehicleInteriorColour(vehicle)
         local customPrimaryColor = nil
         if hasCustomPrimaryColor then
-            customPrimaryColor = { GetVehicleCustomPrimaryColour(vehicle) }
+            customPrimaryColor = {GetVehicleCustomPrimaryColour(vehicle)}
         end
-    
-        local hasCustomXenonColor, customXenonColorR, customXenonColorG, customXenonColorB = GetVehicleXenonLightsCustomColor(vehicle)
+
+        local hasCustomXenonColor, customXenonColorR, customXenonColorG, customXenonColorB =
+            GetVehicleXenonLightsCustomColor(vehicle)
         local customXenonColor = nil
         if hasCustomXenonColor then
-            customXenonColor = { customXenonColorR, customXenonColorG, customXenonColorB }
+            customXenonColor = {customXenonColorR, customXenonColorG, customXenonColorB}
         end
-    
+
         local hasCustomSecondaryColor = GetIsVehicleSecondaryColourCustom(vehicle)
         local customSecondaryColor = nil
         if hasCustomSecondaryColor then
-            customSecondaryColor = { GetVehicleCustomSecondaryColour(vehicle) }
+            customSecondaryColor = {GetVehicleCustomSecondaryColour(vehicle)}
         end
-    
+
         local extras = {}
         for extraId = 0, 20 do
             if DoesExtraExist(vehicle, extraId) then
                 extras[tostring(extraId)] = IsVehicleExtraTurnedOn(vehicle, extraId)
             end
         end
-    
+
         local doorsBroken, windowsBroken, tyreBurst = {}, {}, {}
         local numWheels = tostring(GetVehicleNumberOfWheels(vehicle))
-    
+
         local TyresIndex = { -- Wheel index list according to the number of vehicle wheels.
-            ["2"] = { 0, 4 }, -- Bike and cycle.
-            ["3"] = { 0, 1, 4, 5 }, -- Vehicle with 3 wheels (get for wheels because some 3 wheels vehicles have 2 wheels on front and one rear or the reverse).
-            ["4"] = { 0, 1, 4, 5 }, -- Vehicle with 4 wheels.
-            ["6"] = { 0, 1, 2, 3, 4, 5 }, -- Vehicle with 6 wheels.
+            ["2"] = {0, 4}, -- Bike and cycle.
+            ["3"] = {0, 1, 4, 5}, -- Vehicle with 3 wheels (get for wheels because some 3 wheels vehicles have 2 wheels on front and one rear or the reverse).
+            ["4"] = {0, 1, 4, 5}, -- Vehicle with 4 wheels.
+            ["6"] = {0, 1, 2, 3, 4, 5} -- Vehicle with 6 wheels.
         }
-    
+
         if TyresIndex[numWheels] then
             for _, idx in pairs(TyresIndex[numWheels]) do
                 tyreBurst[tostring(idx)] = IsVehicleTyreBurst(vehicle, idx, false)
             end
         end
-    
+
         for windowId = 0, 7 do -- 13
-            RollUpWindow(vehicle, windowId) --fix when you put the car away with the window down
+            RollUpWindow(vehicle, windowId) -- fix when you put the car away with the window down
             windowsBroken[tostring(windowId)] = not IsVehicleWindowIntact(vehicle, windowId)
         end
-    
+
         local numDoors = GetNumberOfVehicleDoors(vehicle)
         if numDoors and numDoors > 0 then
             for doorsId = 0, numDoors do
                 doorsBroken[tostring(doorsId)] = IsVehicleDoorDamaged(vehicle, doorsId)
             end
         end
-    
+
         return {
             model = GetEntityModel(vehicle),
             doorsBroken = doorsBroken,
@@ -518,35 +514,36 @@ function GetVehicleProperties(vehicle)
             tyresCanBurst = GetVehicleTyresCanBurst(vehicle),
             plate = Trim(GetVehicleNumberPlateText(vehicle)),
             plateIndex = GetVehicleNumberPlateTextIndex(vehicle),
-    
+
             bodyHealth = Round(GetVehicleBodyHealth(vehicle), 1),
             engineHealth = Round(GetVehicleEngineHealth(vehicle), 1),
             tankHealth = Round(GetVehiclePetrolTankHealth(vehicle), 1),
-    
+
             fuelLevel = Round(GetVehicleFuelLevel(vehicle), 1),
             dirtLevel = Round(GetVehicleDirtLevel(vehicle), 1),
             color1 = colorPrimary,
             color2 = colorSecondary,
             customPrimaryColor = customPrimaryColor,
             customSecondaryColor = customSecondaryColor,
-    
+
             pearlescentColor = pearlescentColor,
             wheelColor = wheelColor,
-    
+
             dashboardColor = dashboardColor,
             interiorColor = interiorColor,
-    
+
             wheels = GetVehicleWheelType(vehicle),
             windowTint = GetVehicleWindowTint(vehicle),
             xenonColor = GetVehicleXenonLightsColor(vehicle),
             customXenonColor = customXenonColor,
-    
-            neonEnabled = { IsVehicleNeonLightEnabled(vehicle, 0), IsVehicleNeonLightEnabled(vehicle, 1), IsVehicleNeonLightEnabled(vehicle, 2), IsVehicleNeonLightEnabled(vehicle, 3) },
-    
+
+            neonEnabled = {IsVehicleNeonLightEnabled(vehicle, 0), IsVehicleNeonLightEnabled(vehicle, 1),
+                           IsVehicleNeonLightEnabled(vehicle, 2), IsVehicleNeonLightEnabled(vehicle, 3)},
+
             neonColor = table.pack(GetVehicleNeonLightsColour(vehicle)),
             extras = extras,
             tyreSmokeColor = table.pack(GetVehicleTyreSmokeColor(vehicle)),
-    
+
             modSpoilers = GetVehicleMod(vehicle, 0),
             modFrontBumper = GetVehicleMod(vehicle, 1),
             modRearBumper = GetVehicleMod(vehicle, 2),
@@ -559,23 +556,23 @@ function GetVehicleProperties(vehicle)
             modRightFender = GetVehicleMod(vehicle, 9),
             modRoof = GetVehicleMod(vehicle, 10),
             modRoofLivery = GetVehicleRoofLivery(vehicle),
-    
+
             modEngine = GetVehicleMod(vehicle, 11),
             modBrakes = GetVehicleMod(vehicle, 12),
             modTransmission = GetVehicleMod(vehicle, 13),
             modHorns = GetVehicleMod(vehicle, 14),
             modSuspension = GetVehicleMod(vehicle, 15),
             modArmor = GetVehicleMod(vehicle, 16),
-    
+
             modTurbo = IsToggleModOn(vehicle, 18),
             modSmokeEnabled = IsToggleModOn(vehicle, 20),
             modXenon = IsToggleModOn(vehicle, 22),
-    
+
             modFrontWheels = GetVehicleMod(vehicle, 23),
             modCustomFrontWheels = GetVehicleModVariation(vehicle, 23),
             modBackWheels = GetVehicleMod(vehicle, 24),
             modCustomBackWheels = GetVehicleModVariation(vehicle, 24),
-    
+
             modPlateHolder = GetVehicleMod(vehicle, 25),
             modVanityPlate = GetVehicleMod(vehicle, 26),
             modTrimA = GetVehicleMod(vehicle, 27),
@@ -599,7 +596,7 @@ function GetVehicleProperties(vehicle)
             modTank = GetVehicleMod(vehicle, 45),
             modWindows = GetVehicleMod(vehicle, 46),
             modLivery = GetVehicleMod(vehicle, 48) == -1 and GetVehicleLivery(vehicle) or GetVehicleMod(vehicle, 48),
-            modLightbar = GetVehicleMod(vehicle, 49),
+            modLightbar = GetVehicleMod(vehicle, 49)
         }
     end
 end
